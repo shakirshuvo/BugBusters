13c071eef8303b253d740d7480ab8cba27720777
shakirshuvo <shakirshuvo@hotmail.com>
2020-01-27 19:05:01 -0800

    first commit

--- Generic/src/main/java/base/CommonAPI.java
+++ Generic/src/main/java/base/CommonAPI.java
@@ -1,17 +1,24 @@
 package base;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -21,15 +28,86 @@ import java.util.concurrent.TimeUnit;
 
 public class CommonAPI {
 
-    public static WebDriver driver = null;
 
+    //Browser SetUp
+    public static WebDriver driver = null;
+    public String browserstack_username = "shakirjahangir1";
+    public String browserstack_accesskey = "REz3o29QHkEXvcj7ausZ";
+    public String saucelabs_username = "";
+    public String saucelabs_accesskey = "";
+
+    @Parameters({"useCloudEnv", "cloudEnvName", "os", "os_version", "browserName", "browserVersion", "url"})
     @BeforeMethod
-    @Parameters({"url"})
-    public void setUp(String url) {
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Shakir\\Desktop\\BugBusters\\driver\\geckodriver.exe");
-        driver = new FirefoxDriver();
+    public void setUp(@Optional("false") boolean useCloudEnv, @Optional("false") String cloudEnvName,
+                      @Optional("OS X") String os, @Optional("10") String os_version, @Optional("chrome-options") String browserName, @Optional("34")
+                              String browserVersion, @Optional("http://www.amazon.com") String url) throws IOException {
+        //System.setProperty("webdriver.chrome.driver", "/Users/peoplentech/eclipse-workspace-March2018/SeleniumProject1/driver/chromedriver");
+        if (useCloudEnv == true) {
+            if (cloudEnvName.equalsIgnoreCase("browserstack")) {
+                getCloudDriver(cloudEnvName, browserstack_username, browserstack_accesskey, os, os_version, browserName, browserVersion);
+            } else if (cloudEnvName.equalsIgnoreCase("saucelabs")) {
+                getCloudDriver(cloudEnvName, saucelabs_username, saucelabs_accesskey, os, os_version, browserName, browserVersion);
+            }
+        } else {
+            getLocalDriver(os, browserName);
+        }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
         driver.get(url);
+        //driver.manage().window().maximize();
+    }
+
+    public WebDriver getLocalDriver(@Optional("mac") String OS, String browserName) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            if (OS.equalsIgnoreCase("OS X")) {
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shakir\\Desktop\\BugBusters\\driver\\chromedriver");
+            } else if (OS.equalsIgnoreCase("Windows")) {
+                System.setProperty("webdriver.chrome.driver", "../driver/chromedriver.exe");
+            }
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("chrome-options")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            if (OS.equalsIgnoreCase("OS X")) {
+                System.setProperty("webdriver.chrome.driver", "../driver/chromedriver");
+            } else if (OS.equalsIgnoreCase("Windows")) {
+                System.setProperty("webdriver.chrome.driver", "../driver/chromedriver.exe");
+            }
+            driver = new ChromeDriver(options);
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            if (OS.equalsIgnoreCase("OS X")) {
+                System.setProperty("webdriver.gecko.driver", "C:\\Users\\Shakir\\Desktop\\BugBusters\\driver\\geckodriverr");
+            } else if (OS.equalsIgnoreCase("Windows")) {
+                System.setProperty("webdriver.gecko.driver", "../driver/geckodriver.exe");
+            }
+            driver = new FirefoxDriver();
+
+        } else if (browserName.equalsIgnoreCase("ie")) {
+            System.setProperty("webdriver.ie.driver", "../driver/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+        }
+        return driver;
+
+    }
+
+
+    public WebDriver getCloudDriver(String envName, String envUsername, String envAccessKey, String os, String os_version, String browserName,
+                                    String browserVersion) throws IOException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("browser", browserName);
+        cap.setCapability("browser_version", browserVersion);
+        cap.setCapability("os", os);
+        cap.setCapability("os_version", os_version);
+        if (envName.equalsIgnoreCase("Saucelabs")) {
+            //resolution for Saucelabs
+            driver = new RemoteWebDriver(new URL("http://" + envUsername + ":" + envAccessKey +
+                    "@ondemand.saucelabs.com:80/wd/hub"), cap);
+        } else if (envName.equalsIgnoreCase("Browserstack")) {
+            cap.setCapability("resolution", "1024x768");
+            driver = new RemoteWebDriver(new URL("http://" + envUsername + ":" + envAccessKey +
+                    "@hub-cloud.browserstack.com/wd/hub"), cap);
+        }
+        return driver;
     }
 
     @AfterMethod
@@ -38,13 +116,13 @@ public class CommonAPI {
     }
 
     //helper methods
-    public void clickOnElement(String locator){
+    public void clickOnElement(String locator) {
         try {
             driver.findElement(By.cssSelector(locator)).click();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             try {
                 driver.findElement(By.className(locator)).click();
-            }catch (Exception ex2) {
+            } catch (Exception ex2) {
                 try {
                     driver.findElement(By.id(locator)).click();
                 } catch (Exception ex3) {
@@ -53,13 +131,15 @@ public class CommonAPI {
             }
         }
     }
-    public void typeOnElement(String locator, String value){
+
+    public void typeOnElement(String locator, String value) {
         try {
             driver.findElement(By.cssSelector(locator)).sendKeys(value);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             driver.findElement(By.id(locator)).sendKeys(value);
         }
     }
+
     public static void typeOnElementNEnter(String locator, String value) {
         try {
             driver.findElement(By.cssSelector(locator)).sendKeys(value, Keys.ENTER);
@@ -79,7 +159,7 @@ public class CommonAPI {
         }
     }
 
-    public static void typeOnElementNEnter(String locator, String value,WebDriver driver1) {
+    public static void typeOnElementNEnter(String locator, String value, WebDriver driver1) {
         try {
             driver1.findElement(By.cssSelector(locator)).sendKeys(value, Keys.ENTER);
         } catch (Exception ex1) {
@@ -97,6 +177,7 @@ public class CommonAPI {
             }
         }
     }
+
     public void clearField(String locator) {
         driver.findElement(By.id(locator)).clear();
     }
@@ -137,6 +218,7 @@ public class CommonAPI {
             }
         }
     }
+
     public void typeOnInputField(String locator, String value) {
         try {
             driver.findElement(By.cssSelector(locator)).sendKeys(value);
@@ -211,6 +293,7 @@ public class CommonAPI {
         list = driver1.findElements(By.cssSelector(locator));
         return list;
     }
+
     public List<WebElement> getListOfWebElementsByXpath(String locator) {
         List<WebElement> list = new ArrayList<WebElement>();
         list = driver.findElements(By.xpath(locator));
