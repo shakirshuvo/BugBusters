d652de52de2ef75c211b8d85e8eb5870ee17c18c
Sarah Kh <sarah.kh006@gmail.com>
2020-01-31 20:49:20 -0500

    /add recent code

--- Ebay/src/main/java/home/ebayHomePage.java
+++ Ebay/src/main/java/home/ebayHomePage.java
@@ -7,24 +7,39 @@ public class ebayHomePage extends CommonAPI {
 
 
     @Test(enabled = false)
-    public void searchItem(){
+    public void searchItem() {
 
         System.out.println (driver.getTitle ());
 
         clickOnElement ("//input[@type='text']");
-        typeOnElementNEnter ("//input[@type='text']","iphone11");
+        typeOnElementNEnter ("//input[@type='text']", "iphone11");
         clickOnElement ("//input[@type='submit']");
     }
     //push recent code
 
-    @Test(enabled = true)
+    @Test(enabled = false)
 
-    public void logIn(){
+    public void logIn() {
 
         clickOnElement ("//a[contains(text(),'Sign in')]");
-        typeOnElement ("/a[contains(text(),'Sign in')]","sarah.kh");
-        typeOnElement ("//input[@id='pass']","peoplentech");
+        typeOnElement ("/a[contains(text(),'Sign in')]", "sarah.kh");
+        typeOnElement ("//input[@id='pass']", "peoplentech");
         clickOnElement ("//button[@id='sgnBt']");
+    }
+
+    @Test
+    public void registration() {
+        //System.out.println (driver.getTitle ());
+
+          clickOnElement ("span#gh-ug-flex > a");
+
+          typeOnElement ("input[name='firstname']", "John");
+          typeOnElement ("input[name='lastname']", "Doe");
+          typeOnElement ("input[name='email']", "jdoe12399100@yahoo.com");
+          typeOnElement ("input[name='reg_email_confirmation__']", "jdoe12399100@yahoo.com");
+          typeOnElement ("input#PASSWORD", "peopleNtech");
+          clickOnElement ("//input[@aria-label='Show password']");
+          clickOnElement ("button[id='ppaFormSbtBtn']");
     }
 }
 
