1604a512601cb0270546ac8d974b07cdb9e56b7f
blackcatclaws <BlackCatClaws92@outlook.com>
2020-02-05 14:50:30 -0500

    pull request

--- AliExpress/src/main/java/home/AliExpressHome.java
+++ AliExpress/src/main/java/home/AliExpressHome.java
@@ -1,6 +1,28 @@
 package home;
 
 import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+import reporting.TestLogger;
+
 
 public class AliExpressHome extends CommonAPI {
+
+    @FindBy(id = "Sign in")
+    public WebElement signIn;
+
+    @FindBy(xpath = "//input[@id='fm-login-id']")
+    public WebElement logIn;
+
+    @FindBy(xpath = "//input[@id='fm-login-password']")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//form[@id='login-form']//button[@type='submit']")
+    public WebElement loggingIn;
+
+    //public static WebElement getSignIn() {return SigningIn;}
+
+
+
 }
