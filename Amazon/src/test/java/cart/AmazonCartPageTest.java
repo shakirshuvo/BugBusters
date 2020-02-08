dbc1832263fbb39062d463bf42fbb5ed45f129a9
shakirshuvo <shakirshuvo@hotmail.com>
2020-02-07 16:48:04 -0800

    added tests for AmazonCartTest

--- Amazon/src/test/java/cart/AmazonCartPageTest.java
+++ Amazon/src/test/java/cart/AmazonCartPageTest.java
@@ -6,15 +6,34 @@ import org.testng.annotations.Test;
 
 public class AmazonCartPageTest extends CommonAPI {
 
-    @Test
-    public void testCartTab() {
-        AmazonCartPage amazoncartPage = PageFactory.initElements(driver, AmazonCartPage.class);
-        amazoncartPage.navigateToCartPage();
-    }
+//    @Test
+//    public void testCartTab() {
+//        AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
+//        amazonCartPage.navigateToCartPage();
+//    }
+//
+//    @Test
+//    public void testCartPageTitle() {
+//        AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
+//        amazonCartPage.getCartPageTitle();
+//    }
+//
+//    // This test tests adding WD 2TB Elements Portable External Hard Drive to Amazon cart
+//    @Test
+//    public void testAddWD2TBElementsHDToCart() {
+//        AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
+//        amazonCartPage.addWD2TBElementsHDToCart();
+//    }
+//
+//    @Test
+//    public void testCheckoutWD2TBElementsHDToCart() {
+//        AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
+//        amazonCartPage.checkoutWD2TBElementsHD();
+//    }
 
     @Test
-    public void testCartPageTitle() {
-        AmazonCartPage amazoncartPage = PageFactory.initElements(driver, AmazonCartPage.class);
-        amazoncartPage.getCartPageTitle();
+    public void testQtyDropdown() {
+        AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
+        amazonCartPage.getQtyDropdown();
     }
 }
