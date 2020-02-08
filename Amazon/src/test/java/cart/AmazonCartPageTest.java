e711dc9c9071bf75ac5dfe08a33d40bbcb1da77a
shakirshuvo <shakirshuvo@hotmail.com>
2020-02-07 18:40:55 -0800

    added tests to AmazonCartTest

--- Amazon/src/test/java/cart/AmazonCartPageTest.java
+++ Amazon/src/test/java/cart/AmazonCartPageTest.java
@@ -6,32 +6,38 @@ import org.testng.annotations.Test;
 
 public class AmazonCartPageTest extends CommonAPI {
 
-    @Test
+    @Test (priority = 1)
     public void testCartTab() {
         AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
         amazonCartPage.navigateToCartPage();
     }
 
-    @Test
+    @Test (priority = 2)
     public void testCartPageTitle() {
         AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
         amazonCartPage.getCartPageTitle();
     }
 
     // This test tests adding WD 2TB Elements Portable External Hard Drive to Amazon cart
-    @Test
+    @Test (priority = 3)
     public void testAddWD2TBElementsHDToCart() {
         AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
         amazonCartPage.addWD2TBElementsHDToCart();
     }
 
-    @Test
+    @Test (priority = 4)
     public void testCheckoutWD2TBElementsHDToCart() {
         AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
         amazonCartPage.checkoutWD2TBElementsHD();
     }
 
-    @Test
+    @Test(priority = 5)
+    public void testDeleteWD2TBElementsHDToCart() {
+        AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
+        amazonCartPage.deleteWD2TBElementsHDToCart();
+    }
+
+    @Test (priority = 6)
     public void testQtyDropdown() {
         AmazonCartPage amazonCartPage = PageFactory.initElements(driver, AmazonCartPage.class);
         amazonCartPage.getQtyDropdown();
